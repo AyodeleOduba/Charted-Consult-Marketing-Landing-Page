51b21c56628d6b59fd7008adf53cf60ab04741fd
Ayodeleo73 <56768832+Ayodeleo73@users.noreply.github.com>
2021-03-07 06:47:59 -0600

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,37 +1,84 @@
-// Define Global Variables
+// VARIABLES
+const header = document.querySelector('.pageheader')
+const navbar = document.querySelector('nav');
+const navbarList = document.querySelector('.navbarlist');
 const sections = Array.from(document.querySelectorAll('section'));
-const navbarMenu = document.getElementById('navbarlist');
-let navbarItems = sections.length;
+const fragment = document.createDocumentFragment();
 
-// build the nav
-function getNavbarItem() {
-    for (section of sections) {
-        nameOfSection = section.getAttribute('data-nav');
-        goToSection = section.getAttribute('id');
-        getItem = document.createElement('li');
-        getItem.innerHTML = `<a class="menulink" href="#${goToSection}">${nameOfSection}</a>`;
-        navbarMenu.appendChild(getItem);
+// EVENT LISTENERS
+window.addEventListener('scroll', getActive);
+navbarList.addEventListener('click', scrollToSection);
+window.addEventListener('scroll', hideNavbar, false);
+
+// FUNCTIONS
+
+// The addNavbarItem function creates corresponding navigation item that points to the section ID for each section element.
+function addNavbarItem() {
+  sections.forEach((section, i) => {
+    const navbarItem = document.createElement('li');
+    navbarItem.setAttribute('class', 'menuitem');
+    navbarItem.innerHTML = `<a href=#section${i + 1} class="menulink">${section.dataset.nav}</a>`;
+    fragment.appendChild(navbarItem);
+  });
+}
+addNavbarItem();
+navbarList.appendChild(fragment);
+
+// The hideNavbar function hides the navbar when the user is not scrolling.
+let scrolling = false;
+
+function hideNavbar(e) {
+  if (scrolling !== false) {
+    clearTimeout(scrolling);
+    header.classList.remove('slideup');
+    header.classList.add('slidedown');
+  }
+  scrolling = setTimeout(function () {
+    if (header.matches(':hover')) {
+      header.classList.remove('slideup');
+    } else {
+      header.classList.remove('slidedown');
+      header.classList.add('slideup');
     }
+  }, 800);
 }
 
-// Add class 'active' to section when near top of viewport
-function sectionInViewPort (elem) {
-    let positionOfSection = elem.getBoundingClientRect();
-    return (positionOfSection.top >= 0);
-}
+// When a page loads, the window.onload function provides visibility for the navbar.
+window.onload = function () {
+  header.classList.add('slidedown');
+};
 
+// The getActive function adds a current state to corresponding navigation item, and it adds and removes active class. It highlights section to the user for each section in the viewport.  
 function getActive() {
-    for (section of sections) {
-        if (sectionInViewPort(section)) {
-            if (section.classList.contains('youractiveclass') !=null) {
-                section.classList.add('youractiveclass');
-            }
-        } else { 
-            section.classList.remove('youractiveclass');
-        }
+  sections.forEach(section => {
+    const bounding = section.getBoundingClientRect();
+    const sectionLink = document.querySelector(`a[href="#${section.getAttribute('id')}"]`);
+    const sectionHalfShown = section.offsetTop - (section.offsetHeight * 0.5);
+    const sectionBehind = section.offsetTop + (section.offsetHeight * 0.5);
+    if (
+      (bounding.top >= 0) &&
+      (bounding.left >= 0) &&
+      (Math.floor(bounding.right) <= window.innerWidth) &&
+      (window.pageYOffset > sectionHalfShown) && (window.pageYOffset <= sectionBehind)) {
+      section.classList.add('activeclass');
+      sectionLink.classList.add('current');
+    } else if (window.pageYOffset >= sectionBehind || window.pageYOffset < section.offsetTop) {
+      section.classList.remove('activeclass');
+      sectionLink.classList.remove('current');
     }
+  })
 }
 
-// Scroll to anchor ID using the scroll event
-getNavbarItem();
-document.addEventListener('scroll', getActive);
+// The scrollToSection function scrolls to the corresponding section (whose id is the same as href for the item) for each navigation item clicked.
+function scrollToSection(e) {
+  navbarList.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    e.preventDefault();
+    if (anchor === e.target) {
+      console.log(anchor);
+      document.querySelector(e.target.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth',
+      });
+    }
+  })
+}
+
